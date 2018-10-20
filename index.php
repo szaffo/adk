7692e8026b98fe1794cf0ef0d469526c0b81ec8f
Szabó <martin77szabo@gmail.com>
2018-10-20 10:35:03 +0200

    first push

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+
+ ?>
+
+<html>
+	<head lang="hu">
+		<!-- Chrome, Firefox OS and Opera -->
+		<meta name="theme-color" content="#003c8f">
+		<!-- Windows Phone -->
+		<meta name="msapplication-navbutton-color" content="#003c8f">
+		<!-- iOS Safari -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="#003c8f">
+
+		<meta charset="utf-8">
+		<meta name="author" content="Szabó Martin">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<meta name="description" content="">
+		<meta name="keywords" content=""/>
+		
+		<title>ADK</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+		<header>
+			<div class="head">
+				<div class="hamburger_button"></div>
+				<div class="logo">
+					<h1>ADK</h1>
+				</div>
+			</div>
+		</header>
+		
+
+		<content>
+			<div class="column">
+				<div class="row">
+					<input type="selector">
+					<!-- Location selector -->
+				</div>
+				<div class="row">
+					<div class="line">
+						<div class="time"></div>
+						<div class="buttons"></div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="line">
+						<div class="time"></div>
+						<div class="buttons"></div>
+					</div>
+				</div>
+			</div> <!-- column -->
+			<div class="plus-button"></div>
+		</content>
+
+
+		<footer>
+			<div class="footer">
+				<h2>Szabó Martin 2018</h2>
+			</div>
+		</footer>
+	</body>
+
+
+</html>
