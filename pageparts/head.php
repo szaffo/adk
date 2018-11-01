811c75b4e37b9cdc5e7c90f18f3c21925d9e9cfc
Szabó <martin77szabo@gmail.com>
2018-11-01 14:05:41 +0100

    google analitics added

--- pageparts/head.php
+++ pageparts/head.php
@@ -8,11 +8,20 @@
 
 		<meta charset="utf-8">
 		<meta name="author" content="Szabó Martin">
-		<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"/> -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Ha nem szereted a meglepetéseket, és szeretnéd tudni, hogy mikor moshatszn, nézz be ide!">
+		<meta name="description" content="Foglald le a mosógépet, vagy jelezd, hogy egyedül szeretnél feszíteni a kondiban">
 		<meta name="keywords" content="mosás mosógép biliárd kondi adk ajtósidürer sor sori kollégium asztal konditerem"/>
-		
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-61808002-6"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-61808002-6');
+		</script>
+
 		<title>ADK Füzet</title>
 		<link rel="stylesheet" href="css/style.css">
 	</head>
