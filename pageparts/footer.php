a869d5110d00c55329726d60b21fec28004c3707
Szabó <martin77szabo@gmail.com>
2018-11-01 15:26:36 +0100

    final changes

--- pageparts/footer.php
+++ pageparts/footer.php
@@ -2,12 +2,11 @@
 			<div class="footer">
 				<div class="column column-footer">
 					<div class="footer-col-1">
-					<h2>Szabó Martin 2018</h2>
-				</div>
+						<h2>Szabó Martin 2018</h2>
+					</div>
 				<div class="footer-col-2">
 					<!-- <a href="" class="footer-link">Elérhetőségek</a> -->
-					<!-- <a href="" class="footer-link">Hibabejelentés</a> -->
-				</div>
+					<!-- <a href="https://github.com/szaffo/adk/issues/new" target="_blank" class="footer-link">Hibabejelentés</a> -->
 				</div>
 			</div>
 		</footer>
